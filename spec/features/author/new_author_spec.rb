8542a6a2f8ea98386adc0fb665944d7eaf5de968
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2017-11-03 22:34:24 +0100

    Failing test for #2

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -1,8 +1,22 @@
 require 'rails_helper'
 
-describe "New author page", type: :feature do
-  
-  it "should render withour error" do
+describe 'New author page', type: :feature do
+
+  it 'should render withour error' do
     visit new_author_path
+  end
+
+  it "should display 'New Author'" do
+    visit new_author_path
+
+    expect(page).to have_text 'New Author'
+  end
+
+  it 'should have text input for first name, last name, and homepage' do
+    visit new_author_path
+
+    expect(page).to have_field('First name')
+    expect(page).to have_field('Last name')
+    expect(page).to have_field('Homepage')
   end
 end
