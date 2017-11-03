049c44650486beefa635710c0f5a8434794967d0
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2017-11-03 23:43:09 +0100

    Refactor new_author_spec

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -1,34 +1,31 @@
 require 'rails_helper'
 
 describe 'New author page', type: :feature do
-
   it 'should render withour error' do
     visit new_author_path
   end
 
-  it "should display 'New Author'" do
-    visit new_author_path
+  context 'once rendered' do
+    before :each do
+      visit new_author_path
+    end
 
-    expect(page).to have_text 'New Author'
-  end
+    it "should display 'New Author'" do
+      expect(page).to have_text('New Author')
+    end
 
-  it 'should have a form that can create an author' do
-    visit new_author_path
+    it 'should have a form that can create an author' do
+      expect(page).to have_css("form[action='#{authors_path}'][method=post]")
+    end
 
-    expect(page).to have_css("form[action='#{authors_path}'][method=post]")
-  end
+    it 'should have text input for first name, last name, and homepage' do
+      expect(page).to have_field('First name')
+      expect(page).to have_field('Last name')
+      expect(page).to have_field('Homepage')
+    end
 
-  it 'should have text input for first name, last name, and homepage' do
-    visit new_author_path
-
-    expect(page).to have_field('First name')
-    expect(page).to have_field('Last name')
-    expect(page).to have_field('Homepage')
-  end
-
-  it 'should have a submit button' do
-    visit new_author_path
-
-    expect(page).to have_css "input[type=submit][value='Save Author']"
+    it 'should have a submit button' do
+      expect(page).to have_css("input[type=submit][value='Save Author']")
+    end
   end
 end
