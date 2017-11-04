30eb9de2fca4bf8113fbe49e102e8ca0b3e340c9
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2017-11-04 01:10:33 +0100

    Add author_params test

--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe AuthorsController, type: :controller do
+  it 'should filter author parameters' do
+    mock_params = double()
+    filtered = double()
+    expect(mock_params).to receive(:require).with(:author).and_return(filtered)
+    expect(filtered).to receive(:permit) do |*args|
+      expect(args).to contain_exactly(:first_name, :last_name, :homepage)
+    end
 
+    @controller = AuthorsController.new
+    @controller.instance_exec do
+      @_params = mock_params
+      author_params
+    end
+  end
 end
