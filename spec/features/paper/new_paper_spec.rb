e3401eabb5eee360d6b511cfb0a3562997343aa3
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2017-11-05 21:02:05 +0100

    Add form to new paper page.
    Closes #18

--- spec/features/paper/new_paper_spec.rb
+++ spec/features/paper/new_paper_spec.rb
@@ -10,8 +10,14 @@ describe 'New paper page' do
       visit new_paper_path
     end
 
-    it 'should have a text input for title, venue and year' do
+    it 'should have text input for title, venue, and year' do
+      expect(page).to have_field('Title')
+      expect(page).to have_field('Venue')
+      expect(page).to have_field('Year')
+    end
 
+    it 'should have a submit button' do
+      expect(page).to have_css('input[type=submit]')
     end
   end
 end
