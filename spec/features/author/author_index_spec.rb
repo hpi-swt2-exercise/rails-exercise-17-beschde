00d0d02593751cb25160b1e0ac12376d91e34a6b
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2017-11-05 17:49:34 +0100

    Failing test for #9

--- spec/features/author/author_index_spec.rb
+++ spec/features/author/author_index_spec.rb
@@ -9,5 +9,21 @@ describe 'Author index page', type: :feature do
     before :each do
       visit authors_path
     end
+
+    it "should display 'Authors' in header" do
+      expect(page).to have_xpath("//h1[contains(., 'Authors')]")
+    end
+
+    it 'should contain a table header' do
+      expect(page).to have_xpath("//table/tr/th[contains(., 'Name')]")
+      expect(page).to have_xpath("//table/tr/th[contains(., 'Homepage')]")
+    end
+
+    it 'should list authors in a table' do
+      create(:author)
+      visit authors_path
+      expect(page).to have_xpath("//table/tr/td[contains(., 'Alan Turing')]")
+      expect(page).to have_xpath("//table/tr/td[contains(., 'http://wikipedia.de/Alan_Turing')]")
+    end
   end
 end
