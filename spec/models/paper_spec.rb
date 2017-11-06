e79eff1665a585a5588940f512fab2a17c647ac1
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2017-11-06 17:59:46 +0100

    Failing test for #20

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -5,6 +5,18 @@ describe Paper, type: :model do
     @paper = build(:paper)
   end
 
+  context 'once created' do
+    it 'should not be valid without a title' do
+      paper = Paper.new(venue: 'A venue', year: 2017)
+      expect(paper).to_not be_valid
+    end
+
+    it 'should not be valid with an empty title' do
+      paper = Paper.new(title: '', venue: 'A venue', year: 2017)
+      expect(paper).to_not be_valid
+    end
+  end
+
   it 'should have a title, venue and year' do
     paper = Paper.new(title: 'A paper', venue: 'A venue', year: 2017)
     expect(paper.title).to eq('A paper')
