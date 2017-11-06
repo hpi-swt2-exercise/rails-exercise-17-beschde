e432d64845124bd9eebd8b54618f49532a846471
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2017-11-06 22:49:14 +0100

    Increase readability of paper form test

--- spec/features/paper/_paper_form_spec.rb
+++ spec/features/paper/_paper_form_spec.rb
@@ -14,42 +14,42 @@ shared_examples 'a paper form' do
       fill_in 'Year', with: '2017'
     end
 
-    it 'when the title is empty' do
+    example 'when the title is empty' do
       fill_in 'Title', with: ''
       find('input[type=submit]').click
 
       expect(page).to have_text("Title can't be blank")
     end
 
-    it 'when the venue is empty' do
+    example 'when the venue is empty' do
       fill_in 'Venue', with: ''
       find('input[type=submit]').click
 
       expect(page).to have_text("Venue can't be blank")
     end
 
-    it 'when the year is empty' do
+    example 'when the year is empty' do
       fill_in 'Year', with: ''
       find('input[type=submit]').click
 
       expect(page).to have_text("Year can't be blank")
     end
 
-    it 'when the year is not a number' do
+    example 'when the year is not a number' do
       fill_in 'Year', with: 'a year'
       find('input[type=submit]').click
 
       expect(page).to have_text('Year is not a number')
     end
 
-    it 'when the year is negative' do
+    example 'when the year is negative' do
       fill_in 'Year', with: '-2017'
       find('input[type=submit]').click
 
       expect(page).to have_text('Year must be greater than or equal to 0')
     end
 
-    it 'when the year is not an integer' do
+    example 'when the year is not an integer' do
       fill_in 'Year', with: '2017.42'
       find('input[type=submit]').click
 
