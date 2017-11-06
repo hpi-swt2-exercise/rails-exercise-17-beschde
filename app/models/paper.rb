711a0f6bbd9bf0715a678923c5c23ea42049f059
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2017-11-06 21:57:33 +0100

    Add paper year validation.
    Closes #22

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,4 +1,7 @@
 class Paper < ActiveRecord::Base
   validates :title, presence: true
   validates :venue, presence: true
+  validates :year, presence: true, numericality: {
+    only_integer: true, greater_than_or_equal_to: 0
+  }
 end
