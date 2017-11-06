0e17d03f763d0d436f3d61c291e6ca891d659a7b
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2017-11-06 22:46:38 +0100

    Add failing tests for #23

--- spec/features/paper/_paper_form_spec.rb
+++ spec/features/paper/_paper_form_spec.rb
@@ -7,6 +7,56 @@ shared_examples 'a paper form' do
     expect(page).to have_field('Year')
   end
 
+  context 'should show validation errors' do
+    before :each do
+      fill_in 'Title', with: 'A paper'
+      fill_in 'Venue', with: 'A venue'
+      fill_in 'Year', with: '2017'
+    end
+
+    it 'when the title is empty' do
+      fill_in 'Title', with: ''
+      find('input[type=submit]').click
+
+      expect(page).to have_text("Title can't be blank")
+    end
+
+    it 'when the venue is empty' do
+      fill_in 'Venue', with: ''
+      find('input[type=submit]').click
+
+      expect(page).to have_text("Venue can't be blank")
+    end
+
+    it 'when the year is empty' do
+      fill_in 'Year', with: ''
+      find('input[type=submit]').click
+
+      expect(page).to have_text("Year can't be blank")
+    end
+
+    it 'when the year is not a number' do
+      fill_in 'Year', with: 'a year'
+      find('input[type=submit]').click
+
+      expect(page).to have_text('Year is not a number')
+    end
+
+    it 'when the year is negative' do
+      fill_in 'Year', with: '-2017'
+      find('input[type=submit]').click
+
+      expect(page).to have_text('Year must be greater than or equal to 0')
+    end
+
+    it 'when the year is not an integer' do
+      fill_in 'Year', with: '2017.42'
+      find('input[type=submit]').click
+
+      expect(page).to have_text('Year must be an integer')
+    end
+  end
+
   it 'should have a submit button' do
     expect(page).to have_css('input[type=submit]')
   end
