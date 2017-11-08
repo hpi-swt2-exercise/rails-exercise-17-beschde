4e2a219bc2a5dcfe665c5adf2b5655a93652a5ae
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2017-11-08 16:19:04 +0100

    Refactor controller tests

--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 describe AuthorsController, type: :controller do
   describe '#author_params' do
-    include_examples 'parameter security', method: :author_params, form: :author, attributes: [:first_name, :last_name, :homepage]
+    include_examples 'parameter security',
+                     method: :author_params,
+                     form: :author,
+                     attributes: %i[first_name last_name homepage]
   end
 end
--- spec/controllers/papers_controller_spec.rb
+++ spec/controllers/papers_controller_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 describe PapersController, type: :controller do
   describe '#paper_params' do
-    include_examples 'parameter security', method: :paper_params, form: :paper, attributes: [:title, :venue, :year]
+    include_examples 'parameter security',
+                     method: :paper_params,
+                     form: :paper,
+                     attributes: %i[title venue year author_ids]
   end
 end
